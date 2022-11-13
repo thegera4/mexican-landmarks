6348f0e9f462c000b3747f79a941b1450625c985
thegera4 <84020433+thegera4@users.noreply.github.com>
2022-11-13 13:21:23 -0600

    1st commit

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
 } = process.env;
 
 /*let sequelize = process.env.NODE_ENV === "production" ? 
@@ -33,7 +33,12 @@ new Sequelize(
   { logging: false, native: false }
 );*/
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/LandMex`, {
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/LandMex`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
